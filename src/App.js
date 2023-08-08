2af7e728fdef209081734eab48b91152e44da83a
cw-prathmesh-ghate <113418779+cw-prathmesh-ghate@users.noreply.github.com>
2023-08-08 20:50:30 +0530

    completed all four direction logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import "./style.css";
 import React, { useState } from "react";
 import Squares from "./squares";
@@ -11,12 +11,104 @@ function App() {
     [0, 0, 0, 0],
   ]);
 
-  function userPressesRightArrow() {
+  let listOfEmptySpace = [];
+
+  function gettEmptySpace(gameMatrix) {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (gameMatrix[i][j] === 0) {
+          listOfEmptySpace.push([i, j]);
+        }
+      }
+    }
+  }
+
+  function identifyKeyPress(event) {
+    console.log(
+      `the key you pressed is ${event.key} with keycode ${event.keyCode}`
+    );
+    let key = event.key;
+    if (event.key === "ArrowRight" || event.key === "ArrowDown") {
+      userPressesRightArrow(key);
+    } else if (event.key === "ArrowLeft" || event.key === "ArrowUp") {
+      userPressesLeftArrow(key);
+    }
+  }
+
+  function userPressesLeftArrow(key) {
     const temp = gameMatrix;
 
-    let noofrow = temp.length;
+    if (key === "ArrowUp") {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < i; j++) {
+          const tmp = temp[i][j];
+          temp[i][j] = temp[j][i];
+          temp[j][i] = tmp;
+        }
+      }
+    }
 
-    for (let i = 0; i < noofrow; i++) {
+    for (let i = 0; i < 4; i++) {
+      let rem = temp[i][0];
+      let remindex = 0;
+
+      for (let j = 1; j < 4; j++) {
+        if (temp[i][j] !== 0 && temp[i][j] === rem) {
+          temp[i][remindex] = temp[i][j] + temp[i][remindex];
+          temp[i][j] = 0;
+          rem = temp[i][j];
+        } else if (temp[i][j] !== rem) {
+          if (temp[i][j - 1] === 0) {
+            if (temp[i][j] !== 0) {
+              if (temp[i][remindex] !== 0) {
+                temp[i][remindex + 1] = temp[i][j];
+                rem = temp[i][remindex + 1];
+                remindex = remindex + 1;
+              } else {
+                temp[i][remindex] = temp[i][j];
+                rem = temp[i][remindex];
+              }
+              temp[i][j] = 0;
+            }
+          }
+          if (temp[i][j] !== 0) {
+            rem = temp[i][j];
+            remindex = j;
+          }
+        }
+      }
+    }
+
+    if (key === "ArrowUp") {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < i; j++) {
+          const tmp = temp[i][j];
+          temp[i][j] = temp[j][i];
+          temp[j][i] = tmp;
+        }
+      }
+    }
+
+    setGameMatrix(temp);
+    insertingNewElement();
+  }
+
+  function userPressesRightArrow(key) {
+    const temp = gameMatrix;
+
+    if (key === "ArrowDown") {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < i; j++) {
+          const tmp = temp[i][j];
+          temp[i][j] = temp[j][i];
+          temp[j][i] = tmp;
+        }
+      }
+    }
+
+    // let noofrow = temp.length;
+
+    for (let i = 0; i < 4; i++) {
       let rem = temp[i][temp[i].length - 1];
       let remindex = temp[i].length - 1;
 
@@ -34,6 +126,7 @@ function App() {
                 remindex = remindex - 1;
               } else {
                 temp[i][remindex] = temp[i][j];
+                rem = temp[i][remindex];
               }
               temp[i][j] = 0;
             }
@@ -46,41 +139,61 @@ function App() {
       }
     }
 
+    if (key === "ArrowDown") {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < i; j++) {
+          const tmp = temp[i][j];
+          temp[i][j] = temp[j][i];
+          temp[j][i] = tmp;
+        }
+      }
+    }
+
     setGameMatrix(temp);
+    insertingNewElement();
   }
 
   function insertingNewElement() {
     let randomValue = Math.random();
-    let randomXPos = Math.floor(Math.random() * (3 - 0 + 1) + 0);
-    let randomYPos = Math.floor(Math.random() * (3 - 0 + 1) + 0);
+    gettEmptySpace(gameMatrix);
+    let emptySpaceListLength = listOfEmptySpace.length - 1;
 
-    console.log("X",randomXPos,"Y",randomYPos)
+    let randomNum = Math.floor(
+      Math.random() * (emptySpaceListLength - 0 + 1) + 0
+    );
 
+    let emptySpace = listOfEmptySpace[randomNum];
     let generatedElement = randomValue > 0.5 ? 2 : 4;
 
-    if (gameMatrix[randomXPos][randomYPos] === 0) {
-      const newMat = [...gameMatrix];
-      const newRow = [...newMat[randomXPos]];
-      newRow[randomYPos] = generatedElement;
-      newMat[randomXPos] = newRow;
-      setGameMatrix(newMat);
-    }
+    const newMat = [...gameMatrix];
+    const newRow = [...newMat[emptySpace[0]]];
+    newRow[emptySpace[1]] = generatedElement;
+    newMat[emptySpace[0]] = newRow;
+    setGameMatrix(newMat);
   }
 
-  
+  const ref = useRef(null);
+
+  useEffect(() => {
+    insertingNewElement();
+    ref.current.focus();
+  }, []);
 
   let mySquares = gameMatrix.map((row) => {
     return row.map((data) => {
       return <Squares value={data} />;
     });
   });
-  useEffect(() => {
-    insertingNewElement();
-  }, []);
+
+  // console.log(listOfEmptySpace);
 
   return (
-    <div className="main-container">
-      
+    <div
+      className="main-container"
+      ref={ref}
+      tabIndex={0}
+      onKeyDown={identifyKeyPress}
+    >
       <div className="play-area">{mySquares}</div>
     </div>
   );
--- src/squares.js
+++ src/squares.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 export default function (props) {
+
+  const style= {background: props.value !== 0 ? "wheat" : "rgba(103, 104, 93, 0.377)"}
   return (
     <div className="square-main-container">
       {/* {props.value} */}
-      <div className="individual-grid">{props.value}</div>
+      <div className="individual-grid" style={style}>
+      {props.value ? props.value:""}
+      </div>
     </div>
   );
 }
